d0c57a5fb649613a30fd28137173c332cd3674fd
Laoma Nogueira <laomanogueira@gmail.com>
2021-11-12 15:01:35 -0300

    Exercícios 7 a 11 resolvidos

---
+++ aula4/exercicio10.js
@@ -0,0 +1,6 @@
+//Insira "cachorro" e "brincalhao" no novoArray, imprima-o.
+
+const novoArray = ["gato", "dorminhoco"];
+novoArray.push("cachorro", "brincalhao");
+
+console.log(novoArray);
---
+++ aula4/exercicio11.js
@@ -0,0 +1,7 @@
+//Use a .pop()função para remover o último item meuArray e atribuir o valor exibido a uma nova variável removidoDoArray, imprima ambos.
+
+const nossoArray = ["Fonseca", "Gabriela", "js"];
+let removidoDoArray = nossoArray.pop();
+
+console.log(nossoArray);
+console.log(removidoDoArray);
